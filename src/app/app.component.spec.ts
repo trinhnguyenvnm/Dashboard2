0b1547f96c4d28260e2c86a0d99ab15e5bd4c09b
Trinh Nguyen <trinh.nguyen.vnm@gmail.com>
2018-06-12 11:20:15 +0700

    Create project

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+
+import { AppComponent } from './app.component';
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent} from './shared/navbar/navbar.component';
+import { FixedPluginComponent} from './shared/fixedplugin/fixedplugin.component';
+import {RouterTestingModule} from '@angular/router/testing';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        SidebarComponent,
+        FooterComponent,
+        NavbarComponent,
+        FixedPluginComponent
+      ],
+      imports: [ RouterTestingModule ]
+    }).compileComponents();
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+});
